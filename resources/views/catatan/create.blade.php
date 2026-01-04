26cf445afa325ddfce0a504d5981091e65826ea4
Rogue <blackleaner@gmail.com>
2026-01-04 22:06:33 +0700

    baru

--- resources/views/catatan/create.blade.php
+++ resources/views/catatan/create.blade.php
@@ -37,12 +37,13 @@
                 </label>
 
                 <select name="id_kategori" id="kategori" required>
-                    @foreach ($kategoris as $kategori)
-                        <option value="{{ $kategori->id }}" {{ $SelectedKategori == $kategori->id ? 'selected' : '' }}>
+                    @foreach ( $kategoris as $kategori )
+                        <option value="{{ $kategori->id }}"
+                            {{ $SelectedKategori == $kategori->id ? 'selected' : '' }}>
                             {{ $kategori->jurusan }}
-                        </option>
+                        </option>                      
                     @endforeach
-                </select>
+                    </select>
             </div>
 
             <!-- Isi -->
@@ -58,14 +59,16 @@
 
             {{-- Upload Catatan --}}
             <div class="mb-5">
-                <label class="block text-sm font-medium text-gray-600 mb-2">
-                    Lampiran
-                </label>
+    <label class="block text-sm font-medium text-gray-600 mb-2">
+        Lampiran (opsional)
+    </label>
 
-                <input type="file" name="lampiran[]" multiple
-                    class="w-full px-4 py-2 border border-gray-300 rounded-lg
+    <input type="file"
+           name="lampiran"
+           class="w-full px-4 py-2 border border-gray-300 rounded-lg
                   focus:outline-none focus:ring-2 focus:ring-[#4ED7F1]">
-            </div>
+</div>
+
             <!-- Tombol -->
             <button type="submit"
                 class="w-full bg-[#4ED7F1] hover:bg-[#3BC3DD]
